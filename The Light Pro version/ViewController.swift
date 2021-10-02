14ba82013efc5ff4717263aa196ea0a5b26db461
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-03 01:53:42 +0300

    Added two types of Lights

--- The Light Pro version/ViewController.swift
+++ The Light Pro version/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var isLightOn: Bool = true
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        
     }
-
-
+    
+    
+    @IBAction func buttonPressed1(_ sender: Any) {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        if ((device?.hasTorch) != nil) {
+            do {
+                try device?.lockForConfiguration()
+                if (device?.torchMode == AVCaptureDevice.TorchMode.on) {
+                    device?.torchMode = AVCaptureDevice.TorchMode.off
+                } else {
+                    do {
+                        try device?.setTorchModeOn(level: 1.0)
+                    } catch {
+                        print(error)
+                    }
+                }
+                device?.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    @IBAction func buttonPressed2(_ sender: Any) {
+        isLightOn.toggle()
+        updateColor()
+        
+    }
+    
+    func updateColor() {
+        if isLightOn == true{
+            view.backgroundColor = .white
+        }else{
+            view.backgroundColor = .black
+        }
+    }
 }
 
