09643dd4c1d711519432b6390494a93ade28b4fd
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-03 01:20:41 +0300

    Initial Commit

---
+++ The Light Pro version/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light Pro version
+//
+//  Created by Андрей on 03.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
